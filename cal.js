1b006d18b583ecfbbda069c1f219c804df942297
Norbey Danilo <24207969+ndmc98@users.noreply.github.com>
2024-03-12 14:31:31 -0500

    app

---
+++ cal.js
@@ -0,0 +1,6 @@
+let numero1 = parseFloat(prompt("Introduce el primer número:"));
+let numero2 = parseFloat(prompt("Introduce el segundo número:"));
+
+let suma = numero1 + numero2;
+
+alert("La suma de " + numero1 + " y " + numero2 + " es " + suma + ".");
